c3a9f8625bf8e5c184f891717a72a3c7c5076339
Alonso Ortega <alonsojortega@gmail.com>
2019-01-28 03:37:55 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(array)
   i = 0
   collect = []
   while i < array.length
-    collect << yield(array[i])
-    i+=1
+    collect.push(yield(array[i]))
+    i += 1
   end
   collect
 end
